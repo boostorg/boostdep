4db7fd61275076da7d64933ed13efc98859c2a36
Peter Dimov <pdimov@pdimov.com>
2015-01-08 01:34:56 +0200

    Add --module-weights.

--- examples/report.bat
+++ examples/report.bat
@@ -1,10 +1,14 @@
-REM This is an example cmd.exe batch script
-REM   that uses boostdep.exe to generate a
-REM   complete Boost dependency report.
-REM It needs to be run from the Boost root.
-
-REM git pull
-REM git submodule update --init
+@REM This is an example cmd.exe batch script
+@REM   that uses boostdep.exe to generate a
+@REM   complete Boost dependency report.
+@REM 
+@REM It needs to be run from the Boost root.
+@REM 
+@REM Copyright 2014, 2015 Peter Dimov
+@REM 
+@REM Distributed under the Boost Software License, Version 1.0.
+@REM See accompanying file LICENSE_1_0.txt or copy at
+@REM http://www.boost.org/LICENSE_1_0.txt
 
 SET BOOSTDEP=dist\bin\boostdep.exe
 
@@ -24,5 +28,6 @@ mkdir %OUTDIR%
 
 %BOOSTDEP% --footer "%FOOTER%" --html --module-overview > %OUTDIR%\module-overview.html
 %BOOSTDEP% --footer "%FOOTER%" --html --module-levels > %OUTDIR%\module-levels.html
+%BOOSTDEP% --footer "%FOOTER%" --html --module-weights > %OUTDIR%\module-weights.html
 
 FOR /f %%i IN (%OUTDIR%\list-modules.txt) DO %BOOSTDEP% --title "Dependency Report for %%i" --footer "%FOOTER%" --html --primary %%i --secondary %%i --reverse %%i > %OUTDIR%\%%i.html
