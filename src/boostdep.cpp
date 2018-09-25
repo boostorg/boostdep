deb1487f735edf92722366d74f1fb3cb918e701a
Peter Dimov <pdimov@pdimov.com>
2018-09-26 01:41:12 +0300

    Disable MS security warnings about std::getenv

--- src/boostdep.cpp
+++ src/boostdep.cpp
@@ -7,6 +7,7 @@
 // See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt
 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
